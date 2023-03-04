9ef33df061702ddb25c70eb9b33550104853ffdc
Nuno Barreiro <nuno.barreiro@outlook.com>
2023-03-04 20:04:35 +0000

    move App component to JSX and configure ESLint

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,14 @@
-import React from "react";
 import { createRoot } from "react-dom";
 import Pet from "./Pet";
 
 const App = () => {
-  return React.createElement("div", { id: "app" }, [
-    React.createElement("h1", { id: "title" }, "Adopt Me!"),
-    React.createElement(Pet, {
-      name: "Nala",
-      animal: "Dog",
-      breed: "German Shepperd",
-    }),
-    React.createElement(Pet, {
-      name: "Fluffy",
-      animal: "Cat",
-      breed: "Common European",
-    }),
-  ]);
+  return (
+    <div id="app">
+      <h1>Adopt Me!</h1>
+      <Pet name="Nala" animal="Dog" breed="German shepperd" />
+      <Pet name="Fluffy" animal="Cat" breed="Common European" />
+    </div>
+  );
 };
 
 const container = document.getElementById("root");
@@ -24,4 +17,4 @@ const container = document.getElementById("root");
 // and brings some new concurrency features.
 // no need to ReactDOM.render(container, <App />) unless for legacy reasons
 const root = createRoot(container);
-root.render(React.createElement(App));
+root.render(<App />);
