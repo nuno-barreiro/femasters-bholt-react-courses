6545911c1545dc9dd1f5499db04217632a8cb50b
Nuno Barreiro <nuno.barreiro@outlook.com>
2023-03-06 22:48:08 +0000

    Add animals and breeds UI to search params

--- src/SearchParams.jsx
+++ src/SearchParams.jsx
@@ -1,9 +1,15 @@
 import { useState } from "react";
 
+const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
+const BREEDS = [];
+
 const SearchParams = () => {
   // rule for hooks: have to be created every single time in the same order
   // for example, conditional creation of hooks is not possible
   const [location, setLocation] = useState("");
+
+  const [animal, setAnimal] = useState("");
+  const [breed, setBreed] = useState("");
 
   return (
     <div className="search-params">
@@ -16,6 +22,37 @@ const SearchParams = () => {
             placeholder="eg. Seattle, WA"
             onChange={(evt) => setLocation(evt.target.value)}
           />
+        </label>
+        <label htmlFor="animal">
+          Animal
+          <select
+            id="animal"
+            value={animal}
+            disabled={ANIMALS.length === 0}
+            onChange={(evt) => {
+              setAnimal(evt.target.value);
+              setBreed("");
+            }}
+          >
+            <option />
+            {ANIMALS.map((animal) => (
+              <option key={animal}>{animal}</option>
+            ))}
+          </select>
+        </label>
+        <label htmlFor="breed">
+          Breed
+          <select
+            id="breed"
+            value={breed}
+            disabled={BREEDS.length === 0}
+            onChange={(evt) => setBreed(evt.target.value)}
+          >
+            <option />
+            {BREEDS.map((breed) => (
+              <option key={breed}>{breed}</option>
+            ))}
+          </select>
         </label>
         <button>Submit</button>
       </form>
